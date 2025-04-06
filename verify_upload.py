62091b6f92ca04e8d2d3bc0d6035151a4c5da4ff
Channa Dissanayaka <ywchanna@gmail.com>
2025-04-06 20:06:22 +0530

    .\verify_upload.py

--- verify_upload.py
+++ verify_upload.py
@@ -1,12 +1,33 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access the API key
+GOOGLE_API_KEY = os.getenv("API_KEY")
+
+from google import genai
+from google.genai import types
+
+client = genai.Client(api_key=GOOGLE_API_KEY)
+
+#######################################################
+
 import time
 
-# Check whether the file is ready to be used.
-while video_file.state.name == "PROCESSING":
-    print('.', end='')
-    time.sleep(1)
-    video_file = client.files.get(name=video_file.name)
+def upload_video(video_file_name):
+  video_file = client.files.upload(file=video_file_name)  #sends local video file to Google
 
-if video_file.state.name == "FAILED":
-  raise ValueError(video_file.state.name)
+  while video_file.state == "PROCESSING":
+      print('Waiting for video to be processed.')
+      time.sleep(10)
+      video_file = client.files.get(name=video_file.name)
 
-print('Done')
+  if video_file.state == "FAILED":
+    raise ValueError(video_file.state)
+  print(f'Video processing complete: ' + video_file.uri)
+
+  return video_file
+
+pottery_video = upload_video('GreatRedSpot.mp4')
