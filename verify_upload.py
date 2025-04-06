bf68dad0dd79e0de977abec2f2c462456bc0fabe
Channa Dissanayaka <ywchanna@gmail.com>
2025-04-06 11:55:33 +0530

    .\verify_upload.py

---
+++ verify_upload.py
@@ -0,0 +1,12 @@
+import time
+
+# Check whether the file is ready to be used.
+while video_file.state.name == "PROCESSING":
+    print('.', end='')
+    time.sleep(1)
+    video_file = client.files.get(name=video_file.name)
+
+if video_file.state.name == "FAILED":
+  raise ValueError(video_file.state.name)
+
+print('Done')
